131fa4ff5ed29caa56cec3ad1bbc76a3f989b787
Anton Bondarenko <bondarenko.av24@gmail.com>
2024-08-18 11:53:59 +0300

    фвв дшик вщеутм додав бібліотеку Дотенв

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const WSServer = require('express-ws')(app)
 const aWss = WSServer.getWss()
 const cors = require('cors')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 7310
 const fs = require('fs')
 const path = require('path')
+
+
+
+console.log(
+    process.env.COLOR 
+)
 
 app.use(cors())
 app.use(express.json())
